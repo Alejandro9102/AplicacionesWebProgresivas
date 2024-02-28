39197fcfe0b1b5bd6520ddecfd184c0444097572
Alejandro9102 <105220452+Alejandro9102@users.noreply.github.com>
2024-02-28 11:55:43 -0700

    Add files via upload

--- servwork.js
+++ servwork.js
@@ -1,6 +1,6 @@
 ;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'V1_Pagina_Personal',
   urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
